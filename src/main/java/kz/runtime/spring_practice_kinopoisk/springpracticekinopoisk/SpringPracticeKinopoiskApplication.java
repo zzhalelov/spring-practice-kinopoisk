5a2c7a63905c2408e8a28b2ff10ad01ee29c26f6
Zhastilek Zhalelov <zzhalelov@gmail.com>
2024-05-28 13:52:22 +0500

    feat: create project; add dependencies

---
+++ src/main/java/kz/runtime/spring_practice_kinopoisk/springpracticekinopoisk/SpringPracticeKinopoiskApplication.java
@@ -0,0 +1,13 @@
+package kz.runtime.spring_practice_kinopoisk.springpracticekinopoisk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPracticeKinopoiskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringPracticeKinopoiskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kz/runtime/spring_practice_kinopoisk/springpracticekinopoisk/SpringPracticeKinopoiskApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.runtime.spring_practice_kinopoisk.springpracticekinopoisk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPracticeKinopoiskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
