8e626dad9d0ae69d61a5936139a778b9e87890e9
Zhastilek Zhalelov <zzhalelov@gmail.com>
2024-07-07 22:46:27 +0500

    fix: rename MovieController to MediaController

--- src/main/java/kz/runtime/spring_practice_kinopoisk/controller/MediaController.java
+++ src/main/java/kz/runtime/spring_practice_kinopoisk/controller/MediaController.java
@@ -6,7 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class MovieController {
+public class MediaController {
     @GetMapping("/movies")
     public String showForm(Model model) {
         Media movie = new Media();
