ee24265efc14236cfe4d18a5ac2d9d6499c8da4b
Zhastilek Zhalelov <zzhalelov@gmail.com>
2024-07-16 23:44:25 +0500

    feat: change variable's types; set index.html

--- src/main/java/kz/runtime/spring_practice_kinopoisk/model/Media.java
+++ src/main/java/kz/runtime/spring_practice_kinopoisk/model/Media.java
@@ -3,9 +3,10 @@ package kz.runtime.spring_practice_kinopoisk.model;
 public class Media {
     private String title;
     private Type type;
-    private String releaseYear;
+    private int releaseYear;
     private String country;
     private Genre genre;
     private String ageCategory;
     private String description;
+    private String urlMedia;
 }
